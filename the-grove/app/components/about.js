8c982c9b8577bb8a913a7063febbd5a15e76832e
laurensgalarza <92804326+laurensgalarza@users.noreply.github.com>
2025-05-09 23:50:17 -0400

    final tweaks

--- the-grove/app/components/about.js
+++ the-grove/app/components/about.js
@@ -5,7 +5,7 @@ export default function About() {
 
     return(
 
-        <div className="max-w-[50vw] text-[floralwhite] self-center bg-[rgb(183, 175, 160)]">
+        <div className="text-[floralwhite] self-center bg-[rgb(183, 175, 160)]">
         <h2 className="text-[3em] drop-shadow mb-4">About Us</h2> 
         
         <p className="text-lg">The Grove was born in 1993, when we left Greece and came to the United States. We found that cooking kept us connected to our home in the Mediterranean, and decided we must share our cuisine with our new community. Two years after arriving in Brooklyn, we opened up The Grove, inspired by the olive groves from our childhood. Ever since then, we have been honored to share our love of food and the Mediterranean with you.</p>
